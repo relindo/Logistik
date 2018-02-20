6673166722d154612511e203594620f20fb5cfba
relindo <36653810+relindo@users.noreply.github.com>
2018-02-20 11:36:30 +0700

    Update kenalan.js

--- kenalan.js
+++ kenalan.js
@@ -6,4 +6,4 @@ var nama;
   alert('Hhmm sepertinya anda tidak mau berkenalan dengan saya.');
 }
 
-<script type="text/javascript" src="https://cdn.rawgit.com/relindo/Logistik/23eb3392/kenalan.js"></script>
+
